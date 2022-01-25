82f9df357eed7aae342f3f3d84cdcb193ac3b825
Javier Seco <javier_secotapia@hotmail.com>
2022-01-25 17:34:01 -0300

    Ejercicio Pitagoras

---
+++ Pitagoras.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class Pitagoras {
+    public double calcularHipotenusa(int catetoA, int catetoB) {
+        double cateto1 = 2;
+        double cateto2 = 3;
+        double hypo = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+        return hypo;
+    }
+}
+
---
+++ PitagorasTest.java
@@ -0,0 +1,7 @@
+public class PitagorasTest {
+    public static void main(String[] args) {
+        Pitagoras iD = new Pitagoras();
+        double hypotenusa = iD.calcularHipotenusa(0, 0);
+        System.out.println(hypotenusa);
+    }
+}
