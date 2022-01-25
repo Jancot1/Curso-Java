b6d2f094d809938024f5aebe004aee4c120dfa2f
Javier Seco <javier_secotapia@hotmail.com>
2022-01-25 13:14:00 -0300

    Archivo de Test

---
+++ TestCode.java
@@ -0,0 +1,23 @@
+public class TestCode {
+    public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+    
+        int sum = 0;
+    
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+    
+            sum += i;
+    
+        }
+    
+        System.out.println("Suma: " + sum);
+    
+        long end = System.currentTimeMillis();
+    
+        double total = (double) (end - start) / 1000;
+    
+        System.out.println("Tiempo de ejecucion: " + total + " segundos");
+    
+    }
+}
