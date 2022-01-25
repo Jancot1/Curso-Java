4a85db83282410a9ba132f9c7693b156644bb0a3
Javier Seco <javier_secotapia@hotmail.com>
2022-01-24 23:06:04 -0300

    Primer Programa

---
+++ PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma {
+    public static void main (String[] args) {
+        System.out.println("Mi nombre es Javier");
+        System.out.println("Tengo 22 años");
+        System.out.println("Mi ciudad es Coquimbo, Chile");
+    }
+}
